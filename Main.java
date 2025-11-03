3de403b15c1bf3dc9082b26bfe95d45d61843f2d
felipecartaxo <felipecartaxo1@gmail.com>
2025-11-02 23:52:55 -0300

    Ajustes no Main após alterações na classe Editor

--- Main.java
+++ Main.java
@@ -4,9 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         Editor editor = new Editor();
-        editor.events.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
-        editor.events.subscribe("save", new EmailNotificationListener("admin@example.com"));
-        editor.events.subscribe("save", new SmsNotificationListener("+55 83 9999-9999"));
+
+        //Agora usamos os métodos do Editor ao invés de acessar events diretamente
+        editor.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
+        editor.subscribe("save", new EmailNotificationListener("admin@example.com"));
+        editor.subscribe("save", new SmsNotificationListener("+55 83 9999-9999"));
 
         try {
             editor.openFile("test.txt");
